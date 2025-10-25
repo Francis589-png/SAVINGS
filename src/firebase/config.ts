a473e19d72c9f688f2925798d919b6729dc03be6
App <app042731@gmail.com>
2025-10-25 14:03:13 +0000

    IS THIS THE WAY TO PUT MY KEYS ON NETLIFY

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-3779562519-32e28",
-  "appId": "1:575434011687:web:2540316fc52b5037c04a02",
-  "apiKey": "AIzaSyA24v4iWVhGzWcC6Kq7HJwBd008OK67jjM",
-  "authDomain": "studio-3779562519-32e28.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "575434011687"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
