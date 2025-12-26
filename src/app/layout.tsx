469a83cdd89bdb50c96cb26f8def3faef72ac34d
App <app042731@gmail.com>
2025-12-26 21:01:56 +0000

    add a manifest.. i want to wrap it for windows to publish it on microsof

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,8 @@ export default function RootLayout({
       <head>
         <title>CurrencyTrack</title>
         <meta name="description" content="Track your savings across different currencies." />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#3A6B4B" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
